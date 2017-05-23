dc3c3b6db79f649c2bb73f3c530dad3fe0891633
burakkaratas <bur.karatas@gmail.com>
2017-05-23 22:43:41 +0300

    first commit

---
+++ microservice-starter/src/main/java/com/karatas/learningpath/MicroserviceStarterApplication.java
@@ -0,0 +1,12 @@
+package com.karatas.learningpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceStarterApplication.class, args);
+	}
+}

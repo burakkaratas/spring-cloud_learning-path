87c1e3aef5a3df0fb9223362e1f491efb1fde76d
Burak <burak.karatas@epias.com.tr>
2017-05-24 16:56:04 +0300

    service discovery implementasyon

--- api-gateway/src/main/java/com/karatas/learningpath/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/karatas/learningpath/ApiGatewayApplication.java
@@ -1,4 +1,5 @@
-package com.karatas.learningpath;
+package com.karatas.learningpath
+		;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- client/src/main/java/com/karatas/learningpath/ClientApplication.java
+++ client/src/main/java/com/karatas/learningpath/ClientApplication.java
@@ -1,12 +1,8 @@
 package com.karatas.learningpath;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class ClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClientApplication.class, args);
+//		SpringApplication.run(ClientApplication.class, args);
 	}
 }
